d7e61b556e721319543f4102de727d978de55d20
LaurentDardenne_cp <SND\LaurentDardenne_cp>
2013-01-15 21:04:14 +0000

    Build de la livraison

---
+++ trunk/Tools/Build.ps1
@@ -0,0 +1,14 @@
+﻿#Build.ps1
+#Appel Psake 
+
+Import-Module Psake
+
+Set-Location $PsIonic.Trunk
+ #Affiche les dépendances des tâches sans exécuter le script
+Invoke-psake .\Release.ps1 -doc
+
+ #Exécuter les tâches du  script
+Invoke-psake .\Release.ps1
+
+Write-Warning "Affiche le contenu de la variable `$PSake"
+$psake
