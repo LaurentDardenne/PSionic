e2b8ef7ea21fcc927544636f0e327b18ae2b68c1
LaurentDardenne_cp <SND\LaurentDardenne_cp>
2013-01-19 16:35:20 +0000

    Correction du chemin d'appel

--- trunk/Tools/Build.ps1
+++ trunk/Tools/Build.ps1
@@ -3,11 +3,8 @@
 
 Import-Module Psake
 
-Set-Location $PsIonic.Trunk
- #Affiche les dépendances des tâches sans exécuter le script
-Invoke-psake .\Release.ps1 -doc
+Set-Location $PsIonic.Tools
 
- #Exécuter les tâches du  script
 Invoke-psake .\Release.ps1
 
 Write-Warning "Affiche le contenu de la variable `$PSake"
