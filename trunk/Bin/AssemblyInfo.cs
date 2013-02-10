0d0f846947526c9c6d8f1b2af1bf2d81cdf44e17
LaurentDardenne_cp <SND\LaurentDardenne_cp>
2013-02-10 17:32:33 +0000

    Correction coquille

--- trunk/Bin/AssemblyInfo.cs
+++ trunk/Bin/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("http://psionic.codeplex.com/")]
 [assembly: AssemblyProduct("PSIonicTools. http://psionic.codeplex.com/")]
-[assembly: AssemblyCopyright("Libre de droits pour tous usage non commercial. 2013")]
+[assembly: AssemblyCopyright("Libre de droits pour tout usages non commercial. 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
