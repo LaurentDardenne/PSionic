31062cc6883ee6d30895d3b380fad0ca92884fd6
LaurentDardenne_cp <SND\LaurentDardenne_cp>
2013-02-16 16:25:47 +0000

    Ajout de la génération du fichier de configuration log4net

--- trunk/Setup/PsIonicSetup.ps1
+++ trunk/Setup/PsIonicSetup.ps1
@@ -1,2 +1,22 @@
-#psGet
+﻿#psGet
 #Install-Module
+
+Write-Warning "Under construction..."
+return
+  
+  try {
+  $My=split-path -parent $MyInvocation.MyCommand.Definition
+  Start-Transcript "$My\PsIonicSetup.Log" -ea SilentlyContinue
+  
+   # crée le fichier XML 
+  $Lines=[System.IO.File]::ReadAllText("$PsIonicTools\Log4Net.Config.xml")
+  #bug V3 seul un nom de variable peut être utilisé et pas $var.method()
+  $PsIonicLogsLg4n=$PsIonicLogs.Replace('\','\\') 
+  $ExecutionContext.InvokeCommand.ExpandString($Lines)|
+   Set-Content "$([System.IO.Path]::GetTempPath())\Log4Net.Config.xml" -Encoding UTF8
+  
+
+} 
+Finally {  
+ Stop-Transcript 
+} 
