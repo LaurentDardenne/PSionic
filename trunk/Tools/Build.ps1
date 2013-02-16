e383528d0eec81cfbe9a540dfe81ace71e1dd0c2
LaurentDardenne_cp <SND\LaurentDardenne_cp>
2013-02-16 16:31:37 +0000

    Ajout du test de présence du module Psake

--- trunk/Tools/Build.ps1
+++ trunk/Tools/Build.ps1
@@ -1,11 +1,19 @@
 ﻿#Build.ps1
-#Appel Psake 
+#Construit la version de PsIonic selon la version de Powershell en cours. 
 
-Import-Module Psake
+# Le profile utilisateur (Profile_DevCodePlex.Ps1) doit être chargé
+$PS=@{
+  64="$env:SystemRoot\system32\WindowsPowerShell\v1.0\powershell.exe";
+  32="$env:SystemRoot\syswow64\WindowsPowerShell\v1.0\powershell.exe"
+  #  32.20="$env:SystemRoot\syswow64\WindowsPowerShell\v1.0\powershell.exe -version 2.0"
+}
 
 Set-Location $PsIonic.Tools
 
-Invoke-psake .\Release.ps1
-
-Write-Warning "Affiche le contenu de la variable `$PSake"
-$psake
+try {
+ IMport-Module Psake -EA stop
+ Invoke-Psake .\Release.ps1
+ $psake
+} catch {
+ Throw "Module Psake is unavailable."
+}
