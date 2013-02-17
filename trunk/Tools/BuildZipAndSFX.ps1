7f3084f6e1bb1d83d08bb7d654b7e1b4977091dd
LaurentDardenne_cp <SND\LaurentDardenne_cp>
2013-02-17 21:29:31 +0000

    Construit une archive (.zip) et une archive autoextractible (.exe).

---
+++ trunk/Tools/BuildZipAndSFX.ps1
@@ -0,0 +1,39 @@
+﻿if (-not $currentContext.tasks.default)
+{ Task default -Depends BuildZipAndSFX }
+
+Task BuildZipAndSFX {
+#Construit une archive autoextractible
+ 
+  Pop-Location
+   Set-location $PsIonicLivraison
+   Write-Verbose "Import module in $PsIonicLivraison"
+    #todo dans un job de build ? 
+   Import-Module .\PsIonic.psd1 -Force  
+   Set-Log4NETDebugLevel -Off
+   
+   $ZipFileName="$PsIonicLivraison\PsIonicSetup.zip"
+   
+   $Files="$PsIonicLivraison\*"
+   $ReadOptions = New-Object Ionic.Zip.ReadOptions -Property @{ 
+                    StatusMessageWriter = [System.Console]::Out;
+                  } 
+
+   $Save=@{
+		ExeOnUnpack="Powershell -noprofile -File .\PsIonicSetup.ps1";  
+        Description="Setup for the PsIonic Powershell module"; 
+		ExtractExistingFile=[Ionic.Zip.ExtractExistingFileAction]::OverwriteSilently;
+        NameOfProduct="PSIonic";
+        VersionOfProduct="1.0.0";
+        Copyright='This module is free for non-commercial purposes. Ce module est libre de droits pour tout usages non commercial'
+	}
+   $SaveOptions=New-ZipSfxOptions @Save
+
+   dir $Files|
+     #Crée une archive .zip
+     Compress-ZipFile $ZipFileName
+   #Puis crée une archive autoextractible
+   ConvertTo-Sfx $ZipFileName -Save $SaveOptions -Read $ReadOptions  
+
+  Push-Location
+  Remove-Module PsIonic -Force   
+} #BuildZipAndSFX
