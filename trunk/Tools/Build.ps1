11ef0df68e76d3f6efae1a75214c281ebb5690e2
LaurentDardenne_cp <SND\LaurentDardenne_cp>
2013-02-18 18:09:44 +0000

    Correction de la gestion d'exception

--- trunk/Tools/Build.ps1
+++ trunk/Tools/Build.ps1
@@ -7,12 +7,13 @@ Set-Location $PsIonicTools
 
 try {
  Import-Module Psake -EA stop
- Invoke-Psake .\Release.ps1 -parameters @{"Config"="Debug"} -nologo
- if ($PSVersion -eq "3.0")
- {
-   Powershell -version 2.0 -noprofile -Command {."$env:PsIonic\Tools\Profile_DevCodePlex.Ps1";IPMO Psake; Set-Location $PsIonicTools; Invoke-Psake .\Common.ps1 -parameters @{"Config"="Debug"} -nologo}
- }
- Invoke-Psake .\BuildZipAndSFX.ps1 -parameters @{"Config"="Debug"} -nologo
 } catch {
  Throw "Module Psake is unavailable."
 }
+ 
+ Invoke-Psake .\Release.ps1 -parameters @{"Config"="Debug"} -nologo
+ if ($PSVersion -eq "3.0")
+ {
+   Powershell -version 2.0 -noprofile -Command {."$env:PsIonicProfile\Profile_DevCodePlex.Ps1";IPMO Psake; Set-Location $PsIonicTools; Invoke-Psake .\Common.ps1 -parameters @{"Config"="Debug"} -nologo}
+ }
+ Invoke-Psake .\BuildZipAndSFX.ps1 -parameters @{"Config"="Debug"} -nologo
