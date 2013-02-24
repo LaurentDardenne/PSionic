0d2d65868b7f96c8cf2f72c06e135687185f9783
LaurentDardenne_cp <SND\LaurentDardenne_cp>
2013-02-24 20:34:16 +0000

    Ajout de l'installation du module.

--- trunk/Setup/PsIonicSetup.ps1
+++ trunk/Setup/PsIonicSetup.ps1
@@ -1,21 +1,37 @@
-﻿#psGet
-#Install-Module
-
-Write-Warning "Under construction..."
-return
-  
-  try {
+﻿try {
   $My=split-path -parent $MyInvocation.MyCommand.Definition
   Start-Transcript "$My\PsIonicSetup.Log" -ea SilentlyContinue
-  
-   # crée le fichier XML 
-  $Lines=[System.IO.File]::ReadAllText("$PsIonicTools\Log4Net.Config.xml")
-  #bug V3 seul un nom de variable peut être utilisé et pas $var.method()
-  $PsIonicLogsLg4n=$PsIonicLogs.Replace('\','\\') 
-  $ExecutionContext.InvokeCommand.ExpandString($Lines)|
-   Set-Content "$([System.IO.Path]::GetTempPath())\Log4Net.Config.xml" -Encoding UTF8
-  
 
+  function Test-ExecutionPolicy {
+    if ((Get-ExecutionPolicy) -eq "Restricted") {
+      Throw "Your execution policy has disabled the scripts execution."
+     }
+  }#Test-ExecutionPolicy
+  
+  function Install-Psionic {
+  #Adapted from (new-object Net.WebClient).DownloadString("http://psget.net/GetPsGet.ps1")         
+      $ModulePaths = @($Env:PSModulePath -split ';')
+      $ExpectedUserModulePath = Join-Path -Path ([Environment]::GetFolderPath('MyDocuments')) -ChildPath WindowsPowerShell\Modules
+      $Destination = $ModulePaths | Where-Object { $_ -eq $ExpectedUserModulePath}
+      if (-not $Destination) {
+          $Destination = $ModulePaths | Select-Object -Index 0
+      }
+      New-Item ($Destination + "\PsIonic\") -ItemType Directory -Force | out-null
+      Copy *.*  "$Destination\Psionic" -rec
+  } #Install-Psionic  
+  
+  function New-LG4NetConfigurationFile {  
+    # crée le fichier XML 
+    $Lines=[System.IO.File]::ReadAllText("$my\Log4Net.Config.xml.Template")
+    $TempPath=[System.IO.Path]::GetTempPath()
+    $PsIonicLogsLg4n=$TempPath.Replace('\','\\') #Possible Security Exception 
+    $ExecutionContext.InvokeCommand.ExpandString($Lines)|
+     Set-Content "$TempPath\Log4Net.Config.xml" -Encoding UTF8
+  } #New-LG4NetConfigurationFile
+
+
+  New-LG4NetConfigurationFile
+  Install-Psionic
 } 
 Finally {  
  Stop-Transcript 
