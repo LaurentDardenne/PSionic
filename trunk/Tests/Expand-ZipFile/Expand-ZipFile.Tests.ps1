19afe8ea4d8fada935a4dc4e01122c1f445922a0
MatthewBETTON_cp <SND\MatthewBETTON_cp>
2013-03-02 22:22:03 +0000

    Mise à jour jeux de paramètres pour Expand-ZipFile

--- trunk/Tests/Expand-ZipFile/Expand-ZipFile.Tests.ps1
+++ trunk/Tests/Expand-ZipFile/Expand-ZipFile.Tests.ps1
@@ -4,6 +4,7 @@ $global:sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.",
 $PSionicModule=Get-Module PsIonic
 
 if(Test-Path $global:here\Archive){ rm $global:here\Archive -Recurse -force }
+if(Test-Path $global:here\CryptedArchive){ rm $global:here\CryptedArchive -Recurse -force }
 
   Describe "Expand-ZipFile" {
 
@@ -95,5 +96,88 @@ if(Test-Path $global:here\Archive){ rm $global:here\Archive -Recurse -force }
         }
         $result | should be ($true)
     }
+
+    It "Expand zip file with many parameters from different parameter sets return true (exception)" {
+        try{
+           &$PSionicModule {Expand-ZipFile -File $global:here\Archive.zip -Destination $global:here\Archive -create -Query '*.dll' -From 'Archive\directory' -Flatten -List -ErrorAction Stop}
+        }catch{
+            $result= $_.Exception.Message -match 'Le jeu de paramètres ne peut pas être résolu à l''aide des paramètres nommés spécifiés.'
+        }
+        $result | should be ($true)
+    }
+
+    It "Expand zip file with list parameter return true" {
+        try{
+            $result = $null
+            $result = &$PSionicModule {Expand-ZipFile -File $global:here\Archive.zip -List -ErrorAction Stop}
+        }catch{
+            $result= $false
+        }
+        $result | should be ($true)
+    }
+
+    It "Expand Archive.zip file with passthru parameter return true" {
+        try{
+            $result = $null
+            $result = &$PSionicModule {Expand-ZipFile -File $global:here\Archive.zip -Destination $global:here\Archive -create -Passthru -ErrorAction Stop}
+            if(-not (test-path $global:here\Archive\Archive\test\test1\test2\background.bmp)){
+                throw "Fichier provenant de l'archive non trouvé après extraction"
+            }
+            rm $global:here\Archive -Recurse -Force
+        }catch{
+            $result=$false
+        }
+        $result | should be ($true)
+    }
+
+ # Tests for encrypted archive
+
+     It "Expand encrypted zip file with BAD password return true" {
+        try{
+           &$PSionicModule {Expand-ZipFile -File $global:here\CryptedArchive.zip -Destination $global:here\CryptedArchive -create -Password BADpassword -ErrorAction Stop}
+        }catch{
+            $result= $_.Exception.Message -match 'Mot de passe incorrect'
+        }
+        $result | should be ($true)
+    }
+
+     It "Expand encrypted zip file with good password return true" {
+        try{
+           &$PSionicModule {Expand-ZipFile -File $global:here\CryptedArchive.zip -Destination $global:here\CryptedArchive -create -Password password -ErrorAction Stop}
+           if(-not (test-path $global:here\CryptedArchive\CryptedArchive\Archive\PerfCenterCpl.ico)){
+             throw "Fichier provenant de l'archive non trouvé après extraction"
+           }
+           if(-not (test-path $global:here\CryptedArchive\CryptedArchive\test.txt)){
+             throw "Fichier provenant de l'archive non trouvé après extraction"
+           }
+           rm $global:here\CryptedArchive -Recurse -Force
+           $result=$true
+        }catch{
+            $result=$false
+        }
+        $result | should be ($true)
+    }
+
+    It "Expand .ico file from encrypted zip file with good password return true" {
+        try{
+           &$PSionicModule {Expand-ZipFile -File $global:here\CryptedArchive.zip -Destination $global:here\CryptedArchive -create -Query '*.ico' -Password password -ErrorAction Stop}
+           $icoFile = Get-ChildItem $global:here\CryptedArchive -filter *.ico -recurse
+           $Files =  Get-ChildItem $global:here\CryptedArchive -recurse
+           if($icoFile.count -ne 1 -or $Files.count -ne 3){
+             throw "Les fichiers n'ont pas été extraits"
+           }
+           if(-not (test-path $global:here\CryptedArchive\CryptedArchive\Archive\PerfCenterCpl.ico)){
+             throw "Le fichier .ico n'a pas été extrait"
+           }
+           rm $global:here\CryptedArchive -Recurse -Force
+           $result=$true
+        }catch{
+            $result=$_.Exception.Message
+        }
+        $result | should be ($false)
+    }
+
+    # TODO : Follow, Interactive
+
   }
 
