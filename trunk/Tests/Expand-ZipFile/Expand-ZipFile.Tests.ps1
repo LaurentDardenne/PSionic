80f20214fc63724ab9701d0937bdbe5069023587
MatthewBETTON_cp <SND\MatthewBETTON_cp>
2013-03-02 21:36:22 +0000

    Mise à jour jeux de tests pour Expand-ZipFile

--- trunk/Tests/Expand-ZipFile/Expand-ZipFile.Tests.ps1
+++ trunk/Tests/Expand-ZipFile/Expand-ZipFile.Tests.ps1
@@ -25,7 +25,7 @@ if(Test-Path $global:here\Archive){ rm $global:here\Archive -Recurse -force }
         $result | should be ($true)
     }
 
-    It "Expand a file that is not a zip file" {
+    It "Expand a file that is not a zip file return true" {
         try{
            &$PSionicModule {Expand-ZipFile -File $global:here\PerfCenterCpl.ico -Destination $global:here\Archive -Create -ErrorAction Stop}
         }catch{
@@ -48,11 +48,45 @@ if(Test-Path $global:here\Archive){ rm $global:here\Archive -Recurse -force }
         $result | should be ($true)
     }
 
-    It "Expand Archive.zip file return true" {
+    It "Expand dll files from Archive.zip file return true" {
         try{
-           &$PSionicModule {Expand-ZipFile -File $global:here\Archive.zip -Destination $global:here\Archive -create -ErrorAction Stop}
-           if(-not (test-path $global:here\Archive\Archive\test\test1\test2\background.bmp)){
-             throw "Fichier provenant de l'archive non trouvé après extraction"
+           &$PSionicModule {Expand-ZipFile -File $global:here\Archive.zip -Destination $global:here\Archive -create -Query '*.dll' -ErrorAction Stop}
+           $Dllfiles = Get-ChildItem $global:here\Archive -filter *.dll -recurse
+           $Files =  Get-ChildItem $global:here\Archive -recurse
+           if($Dllfiles.count -ne 2 -or $Files.count -ne 2){
+             throw "Les 2 fichiers .dll n'ont pas été extraits"
+           }
+           rm $global:here\Archive -Recurse -Force
+           $result=$true
+        }catch{
+            $result=$false
+        }
+        $result | should be ($true)
+    }
+
+    It "Expand one dll file from a subdirectory in zip file return true" {
+        try{
+           &$PSionicModule {Expand-ZipFile -File $global:here\Archive.zip -Destination $global:here\Archive -create -Query '*.dll' -From 'Archive\directory' -ErrorAction Stop}
+           $Dllfiles = Get-ChildItem $global:here\Archive -filter *.dll -recurse
+           $Files =  Get-ChildItem $global:here\Archive -recurse
+           if($Dllfiles.count -ne 1 -or $Files.count -ne 3){
+             throw "Le fichier .dll n'a pas été extrait"
+           }
+           rm $global:here\Archive -Recurse -Force
+           $result=$true
+        }catch{
+            $result=$false
+        }
+        $result | should be ($true)
+    }
+    
+    It "Expand one dll file from a subdirectory in zip file with flatten return true" {
+        try{
+           &$PSionicModule {Expand-ZipFile -File $global:here\Archive.zip -Destination $global:here\Archive -create -Query '*.dll' -From 'Archive\directory' -Flatten -ErrorAction Stop}
+           $Dllfiles = Get-ChildItem $global:here\Archive -filter *.dll -recurse
+           $Files =  Get-ChildItem $global:here\Archive -recurse
+           if($Dllfiles.count -ne 1 -or $Files.count -ne 1){
+             throw "Le fichier .dll n'a pas été extrait"
            }
            rm $global:here\Archive -Recurse -Force
            $result=$true
@@ -62,3 +96,4 @@ if(Test-Path $global:here\Archive){ rm $global:here\Archive -Recurse -force }
         $result | should be ($true)
     }
   }
+
