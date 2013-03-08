4db9db664c2b40a8ac3fb5b57d359b8a1102e58d
LaurentDardenne_cp <SND\LaurentDardenne_cp>
2013-03-08 19:23:45 +0000

    Fonctions complémentaires au script Helps (https://github.com/nightroman/Helps)

---
+++ trunk/Tools/New-CommandHelp.ps1
@@ -0,0 +1,41 @@
+﻿Function New-CommandHelp {
+ param (
+   [Parameter(Mandatory=$true,ValueFromPipeline = $true)]
+   [ValidateNotNullOrEmpty()]
+  [string]$CommandName,
+  
+    [Parameter(Position=0,Mandatory=$true)]
+    [ValidateNotNullOrEmpty()]
+  [string]$ModuleName,
+  
+    [Parameter(Position=1,Mandatory=$true)]
+    [ValidateNotNullOrEmpty()]
+    [ValidateScript( {Test-Path $_})]
+  [string] $TargetDirectory,
+  
+    [Parameter(Position=2,Mandatory=$true)]
+    [ValidateNotNull()]
+  [System.Globalization.CultureInfo]$Culture,
+    
+  [Switch] $All
+  
+ )
+
+ Process {
+    Write-Verbose "[$Culture] create files help for the command : $CommandName"
+    $WorkingDirectory="$TargetDirectory\$CommandName" 
+     #Crée les fichiers finaux de l'aide, préfixé du nom de culture
+    $FileDataBloc="$WorkingDirectory\{0}.{1}.Datas.Template.ps1" -F $ModuleName,$CommandName
+    $NewFileDataBloc="$WorkingDirectory\{0}.{1}.{2}.Datas.ps1" -F $Culture,$ModuleName,$CommandName
+    Write-verbose "Write $NewFileDataBloc"
+    Copy $FileDataBloc $NewFileDataBloc
+    
+    if ($All) 
+    {
+      $FileCmdBloc="$WorkingDirectory\{0}.{1}.Cmds.Template.ps1" -F $ModuleName,$CommandName
+      $NewFileCmdBloc="$WorkingDirectory\{0}.{1}.{2}.Cmds.ps1" -F $Culture,$ModuleName,$CommandName
+      Write-verbose "Write $NewFileCmdBloc"
+      Copy $FileCmdBloc $NewFileCmdBloc
+    } 
+ }#process
+} #New-CommandHelp
