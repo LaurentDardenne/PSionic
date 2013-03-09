fc4447ef58343bc7065f0056b8786e8e0a7a1c10
LaurentDardenne_cp <SND\LaurentDardenne_cp>
2013-03-09 17:24:04 +0000

    Correction de la gestion du pipeline

--- trunk/Tools/ConvertTo-XmlHelp.ps1
+++ trunk/Tools/ConvertTo-XmlHelp.ps1
@@ -1,6 +1,6 @@
 ﻿Function ConvertTo-XmlHelp {
  param (
-   [Parameter(Mandatory=$true,ValueFromPipeline = $true)]
+   [Parameter(Mandatory=$true,ValueFromPipeline = $true,ValueFromPipelineByPropertyName = $true)]
    [ValidateNotNullOrEmpty()]
    [Alias('Key')]
   [string]$CommandName,
