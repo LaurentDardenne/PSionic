2dce790e95d2d021ea0d66ce3813e57474d9d9fd
LaurentDardenne_cp <SND\LaurentDardenne_cp>
2014-01-21 22:29:20 +0000

    Complétion

--- trunk/Documentation/Helps/New-ProgressBarInformations/fr-FR.PSionic.New-ProgressBarInformations.Datas.ps1
+++ trunk/Documentation/Helps/New-ProgressBarInformations/fr-FR.PSionic.New-ProgressBarInformations.Datas.ps1
@@ -1,10 +1,17 @@
 ﻿# New-ProgressBarInformations command data
 $Datas = @{
 	NewProgressBarInformationsSynopsis = "Créer un objet de déclaration d'une barre de progression PSionic standardisée."
-	NewProgressBarInformationsDescription = ''
+	NewProgressBarInformationsDescription = 'Cet objet est utilisé par la fonction New-ReadOptions.'
 	NewProgressBarInformationsSets__AllParameterSets = ''
-	NewProgressBarInformationsParametersactivity = ''
-	NewProgressBarInformationsParametersactivityId = ''
+	NewProgressBarInformationsParametersactivity = @"
+Indique la première ligne de texte dans le titre de la barre de progression. 
+Ce texte décrit l'activité dont la progression est rapportée.
+"@
+	NewProgressBarInformationsParametersactivityId =@"
+ Indique un identifiant distinguant chaque barre de progression. 
+ Utilisez ce paramètre lorsque vous créez plusieurs barres de progression en une seule commande. 
+ Si les barres de progression n'ont pas d'identifiant différent, elles seront superposées au lieu d'être affichées l'une en dessous de l'autre.
+"@
 	NewProgressBarInformationsInputsDescription1 = ''
 	NewProgressBarInformationsOutputsDescription1 = ''
 	NewProgressBarInformationsNotes = ''
