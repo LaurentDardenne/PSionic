6e43a8bdc99993315d2f675425c408148d0deb5b
LaurentDardenne_cp <SND\LaurentDardenne_cp>
2014-01-22 00:35:56 +0000

    Démo utilisant un générateur de filtres de recherche.

---
+++ trunk/Demo/Compress-ZipFile/CompressFile-ByDate.ps1
@@ -0,0 +1,9 @@
+﻿# $FactoryFilterDate= New-FactoryFilterDate -PropertyName "LastWriteTime"
+# $PatternDate="{0:$((Get-Culture).DateTimeFormat.ShortDatePattern)}"
+# $YesterdayFiles=$FactoryFilterDate.ThisDate($PatternDate -F [system.DateTime]::Today.AddDays(-1))
+# dir -rec | ? $code
+
+
+$FileInfoLib= New-FilterLib (New-FactoryFilterDate)
+$FileInfoLib
+Dir |Where $FileInfoLib.Yesterday
