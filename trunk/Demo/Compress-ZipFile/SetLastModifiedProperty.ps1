676030f6acc65a7bec61e5b3ecc5a4f1306ce862
LaurentDardenne_cp <SND\LaurentDardenne_cp>
2014-02-04 17:48:24 +0000

    Fonction Compress-ZipFile : usage du paramètre -SetLastModifiedProperty

---
+++ trunk/Demo/Compress-ZipFile/SetLastModifiedProperty.ps1
@@ -0,0 +1,36 @@
+﻿$SbUTnewest={
+  $fixedTimestamp = New-Object System.DateTime(1601,1,1,0,0,0)
+  foreach($entry in $ZipFile)
+  {
+    if ($entry.LastModified -gt $fixedTimestamp)
+    { $fixedTimestamp = $entry.LastModified }
+  }
+  
+  foreach($entry in $zipFile)
+  { $entry.LastModified = $fixedTimestamp }
+}
+
+$sbUToldest={
+  $fixedTimestamp = "10/01/2014 16:37" -as [Datetime]             
+
+  foreach($entry in $ZipFile)
+  {
+    if ($entry.LastModified -lt $fixedTimestamp)
+    { $fixedTimestamp = $entry.LastModified }
+  }
+
+  foreach($entry in $ZipFile)
+  { $entry.LastModified = $fixedTimestamp }
+}
+
+$sbTimestamp={
+  $fixedTimestamp = [Datetime]::Now              
+  
+  foreach($entry in $ZipFile)
+  { $entry.LastModified = $fixedTimestamp }
+}
+
+#todo chemins des fichiers de test
+Get-ChildItem C:\temp\PsIonic  -Exclude *.zip,*.exe -rec|
+ Where {!$_.psiscontainer}| 
+ Compress-ZipFile -OutputName C:\temp\Archive.zip -ErrorAction Stop -SetLastModifiedProperty $sbUToldest
