2a802011e1ccee7151583113adb5bf47b2e24a17
LaurentDardenne_cp <SND\LaurentDardenne_cp>
2014-02-06 19:48:30 +0000

    Fonction de configuration SFX personnel externe au module.

---
+++ trunk/Get-PsIonicDefaultSfxConfiguration.ps1
@@ -0,0 +1,29 @@
+﻿function Get-PsIonicDefaultSfxConfiguration {
+#Create a personnal SFX configuration
+#
+#When this function exists before importing the module psionic, 
+#it becomes the default configuration. See Get-PsIonicSfxOptions function. 
+
+  $SfxOptions=@{
+    ExeOnUnpack="Powershell -noprofile -File .\MySetup.ps1"
+    ExtractDirectory='%TEMP%'
+    Description='Todo'
+    FileVersion='1.0'
+    IconFile="$(($env:PSModulePath -split ';')[0])\PsIonic\PsIonic.ico"
+    ExtractExistingFile='OverwriteSilently'
+    NameOfProduct='Test'
+    VersionOfProduct='1.0'
+    Copyright='Auteur Laurent Dardenne.'
+
+     #Additional options for the csc.exe compiler, when producing the SFX
+     #see : http://msdn.microsoft.com/en-us/library/6s2x2bzy.aspx
+    AdditionalCompilerSwitches= '/optimize'
+    
+    #WindowTitle='' with -GUI
+    Quiet=$false #switch parameter
+    Remove=$false #switch parameter
+  }
+ New-ZipSfxOptions @SfxOptions  -Cmdline # -Cmdline or -GUI
+}#Get-DefaultSfxConfiguration
+
+
