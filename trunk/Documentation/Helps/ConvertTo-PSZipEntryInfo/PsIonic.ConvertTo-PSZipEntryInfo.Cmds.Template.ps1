f9727050fde4207a39f9b4d7c0caa431f85ff48a
LaurentDardenne_cp <SND\LaurentDardenne_cp>
2014-02-07 17:23:49 +0000

    Conversion la propriété Info d'un objet de type ZipFile ou ZipEntry.

---
+++ trunk/Documentation/Helps/ConvertTo-PSZipEntryInfo/PsIonic.ConvertTo-PSZipEntryInfo.Cmds.Template.ps1
@@ -0,0 +1,36 @@
+# ConvertTo-PSZipEntryInfo command help
+@{
+	command = 'ConvertTo-PSZipEntryInfo'
+	synopsis = $Datas.ConvertToPSZipEntryInfoSynopsis
+	description = $Datas.ConvertToPSZipEntryInfoDescription
+	parameters = @{
+		Info = $Datas.ConvertToPSZipEntryInfoParametersInfo
+	}
+	inputs = @(
+		@{
+			type = ''
+			description = $Datas.ConvertToPSZipEntryInfoInputsDescription1
+		}
+	)
+	outputs = @(
+		@{
+			type = ''
+			description = $Datas.ConvertToPSZipEntryInfoOutputsDescription1
+		}
+	)
+	notes = $Datas.ConvertToPSZipEntryInfoNotes
+	examples = @(
+		@{
+			#title = ''
+			#introduction = ''
+			code = {
+			}
+			remarks = $Datas.ConvertToPSZipEntryInfoExamplesRemarks1
+			test = { . $args[0] }
+		}
+	)
+	links = @(
+		@{ text = ''; URI = '' }
+	)
+}
+
