8d1e4dbe28b9ac4e0c1b9baf7aaecfbe8ac4d8dd
LaurentDardenne_cp <SND\LaurentDardenne_cp>
2014-02-07 17:23:49 +0000

    Conversion la propriété Info d'un objet de type ZipFile ou ZipEntry.

---
+++ trunk/Documentation/Helps/ConvertTo-PSZipEntryInfo/PsIonic.ConvertTo-PSZipEntryInfo.Datas.Template.ps1
@@ -0,0 +1,13 @@
+# ConvertTo-PSZipEntryInfo command data
+$Datas = @{
+	ConvertToPSZipEntryInfoSynopsis = ''
+	ConvertToPSZipEntryInfoDescription = ''
+	ConvertToPSZipEntryInfoSets__AllParameterSets = ''
+	ConvertToPSZipEntryInfoParametersInfo = ''
+	ConvertToPSZipEntryInfoInputsDescription1 = ''
+	ConvertToPSZipEntryInfoOutputsDescription1 = ''
+	ConvertToPSZipEntryInfoNotes = ''
+	ConvertToPSZipEntryInfoExamplesRemarks1 = ''
+}
+
+
---
+++ trunk/Documentation/Helps/ConvertTo-PSZipEntryInfo/fr-FR.PsIonic.ConvertTo-PSZipEntryInfo.Datas.ps1
@@ -0,0 +1,16 @@
+﻿# ConvertTo-PSZipEntryInfo command data
+$Datas = @{
+	ConvertToPSZipEntryInfoSynopsis = 'Converti la propriété Info d''un objet de type ZipFile ou ZipEntry.'
+	ConvertToPSZipEntryInfoDescription = 'Converti la propriété Info d''un objet de type ZipFile ou ZipEntry en une liste d''objets personnalisés'
+	ConvertToPSZipEntryInfoSets__AllParameterSets = ''
+	ConvertToPSZipEntryInfoParametersInfo = ''
+	ConvertToPSZipEntryInfoInputsDescription1 = ''
+	ConvertToPSZipEntryInfoOutputsDescription1 = ''
+	ConvertToPSZipEntryInfoNotes = @"
+La transformation de la propriété Info de type texte en plusieurs objets prend un certains temps.
+Eviter de transformer cette propriété au sein d'une boucle.
+"@
+	ConvertToPSZipEntryInfoExamplesRemarks1 = ''
+}
+
+
