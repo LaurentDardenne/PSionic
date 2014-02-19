8dd85d7e700e86fd1a4506ec625e39f31bcdecdd
LaurentDardenne_cp <SND\LaurentDardenne_cp>
2014-02-19 12:02:04 +0000

    Ajout du comportement List. Suppression du paramètre TempLocation.

--- trunk/Documentation/Helps/Get-ZipFile/fr-FR.PsIonic.Get-ZipFile.Datas.ps1
+++ trunk/Documentation/Helps/Get-ZipFile/fr-FR.PsIonic.Get-ZipFile.Datas.ps1
@@ -6,9 +6,12 @@ $Datas = @{
 	GetZipFileSetsReadOption = ''
 	GetZipFileParametersEncoding = 'Type d''encodage de l''archive. L''utilisation de la valeur par défaut est recommandée.'
 	GetZipFileParametersEncryption = 'Type de cryptage utilisé lors de l''opération de compression. Nécessite de préciser un mot de passe (cf. le paramètre Password).'
+	GetZipFileParametersList = @"
+Obtient les entrées contenu dans le catalogue de l'archive. Les objets renvoyés sont des objets personnalisés dont le nom de type est PSZipEntry.
+"@
 	GetZipFileParametersProgressID =@"
 L'usage de ce paramètre crée un gestionnaire d'événements pour les opérations de lecture.
-Lors de l'ouverture d'une archive zip de grande taille, vous pouvez choisir d'afficher une barre de progression standardisé.
+Lors de l'ouverture d'une archive zip de grande taille, vous pouvez choisir d'afficher une barre de progression standardisée.
 Cette ID permet de distinguer la barre de progression interne des autres. Utilisez ce paramètre lorsque vous créez plus d'une barre de progression.
 "@
 	GetZipFileParametersPath = 'Nom du ou des fichiers ZIP à lire. Vous pouvez préciser des noms de fichier comportant des jokers.'
@@ -16,7 +19,6 @@ Cette ID permet de distinguer la barre de progression interne des autres. Utilis
 	GetZipFileParametersPassword = 'Précise le mot de passe nécessaire à la lecture d''une archive encryptée.'
 	GetZipFileParametersReadOptions = 'Objet d''option de lecture créé à l''aide de la fonction New-ReadOptions.'
 	GetZipFileParametersSortEntries = 'Tri les entrées avant de les envoyer dans le pipeline.'
-	GetZipFileParametersTempLocation = 'Nom du répertoire temporaire utilisé lors de l''extraction de l''archive. Par défaut récupère le contenu de la variable d''environnement %TEMP%.'
 	GetZipFileParametersUnixTimeFormat = 'Le format de date des fichiers sera celui d''Unix'
 	GetZipFileParametersWindowsTimeFormat = 'Le format de date des fichiers sera celui de Windows'
 	GetZipFileParametersZipErrorAction = 'Précise le mode de gestion des erreurs.'
