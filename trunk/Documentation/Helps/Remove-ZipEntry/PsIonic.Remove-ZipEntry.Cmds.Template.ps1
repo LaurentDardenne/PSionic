3358e9c3cc269a32fec9cc518c9c268e0f452a27
LaurentDardenne_cp <SND\LaurentDardenne_cp>
2014-02-21 19:34:38 +0000

    Ebauche de la documentation de la fonction Remove-ZipEntry

---
+++ trunk/Documentation/Helps/Remove-ZipEntry/PsIonic.Remove-ZipEntry.Cmds.Template.ps1
@@ -0,0 +1,44 @@
+﻿# Remove-ZipEntry command help
+@{
+	command = 'Remove-ZipEntry'
+	synopsis = $Datas.RemoveZipEntrySynopsis
+	description = $Datas.RemoveZipEntryDescription
+	sets = @{
+		Name = $Datas.RemoveZipEntrySetsName
+		Query = $Datas.RemoveZipEntrySetsQuery
+	}
+	parameters = @{
+		From = $Datas.RemoveZipEntryParametersFrom
+		InputObject = $Datas.RemoveZipEntryParametersInputObject
+		Name = $Datas.RemoveZipEntryParametersName
+		Query = $Datas.RemoveZipEntryParametersQuery
+		ZipFile = $Datas.RemoveZipEntryParametersZipFile
+	}
+	inputs = @(
+		@{
+			type = ''
+			description = $Datas.RemoveZipEntryInputsDescription1
+		}
+	)
+	outputs = @(
+		@{
+			type = 'System.Management.Automation.PSObject'
+			description = $Datas.RemoveZipEntryOutputsDescriptionSystemManagementAutomationPSObject
+		}
+	)
+	notes = $Datas.RemoveZipEntryNotes
+	examples = @(
+		@{
+			#title = ''
+			#introduction = ''
+			code = {
+			}
+			remarks = $Datas.RemoveZipEntryExamplesRemarks1
+			test = { . $args[0] }
+		}
+	)
+	links = @(
+		@{ text = ''; URI = '' }
+	)
+}
+
