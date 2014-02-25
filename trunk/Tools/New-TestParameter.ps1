74dceb51590dba277a171fe1235e5c2d1d1d5caf
LaurentDardenne_cp <SND\LaurentDardenne_cp>
2014-02-25 20:40:24 +0000

    Ajout des fonctions de création de test :  Get-CommonParameters,Get-ParameterSet, ConvertTo-VariableDeclaration
    Modification de la signature de la fonction New-TestSetParameters

--- trunk/Tools/New-TestParameter.ps1
+++ trunk/Tools/New-TestParameter.ps1
@@ -10,12 +10,13 @@
   [System.Management.Automation.CommandInfo] $CommandName,
     
     [ValidateNotNullOrEmpty()]
-    [Parameter(Position=0,Mandatory=$false,ParameterSetName="Nammed")]
+    [Parameter(Position=0,Mandatory=$false)]
   [string[]] $ParameterSetNames='__AllParameterSets',
     
     [ValidateNotNullOrEmpty()]
-    [Parameter(Position=0,Mandatory=$false,ParameterSetName="All")]
+    [Parameter(Mandatory=$false,ParameterSetName="All")]
   [string[]] $Exclude,
+  
     [Parameter(ParameterSetName="All")]
   [switch] $All
 )       
@@ -173,4 +174,142 @@ $Template=@'
     }
 '@
 $Result.Lines| % { $ExecutionContext.InvokeCommand.ExpandString($Template) }
+#>
+
+function Get-CommonParameters{ 
+ [System.Management.Automation.Internal.CommonParameters].GetProperties()|
+  Foreach {$_.name}
+}#Get-CommonParameters
+
+function Get-ParameterSet {
+#renvoi à partir de $Command 
+#les paramètres du jeux de paramètre indiqué
+ param (
+    [ValidateNotNull()]
+    [Parameter(Mandatory=$True,ValueFromPipeline=$True)]
+  [System.Management.Automation.CommandInfo] $Command,    
+    
+    [ValidateNotNullOrEmpty()]
+    [Parameter(Position=1,Mandatory=$false)]
+  [string]$ParameterSetName='__AllParameterSets',
+  
+  [switch] $List
+  
+ )  
+   process {       
+    if ( ($ParameterSetName -eq '__AllParameterSets') -and ($Command.ParameterSets.Count -gt 1) )
+    { throw "La commande '$($Command.Name)' posséde plusieurs jeux de paramètre, précisez un des noms suivants : $($ofs=' , '; $cmd.ParameterSets|select -expand name)" }
+
+    $I=0
+    $Index=$Command.ParameterSets|? {$I++;$_.name -eq $ParameterSetName}|% { $I-1}
+    if ($i -eq -1 -or $index -eq $null) 
+    {throw "Le jeu de paramètre nommé '$ParameterSetName' n'existe pas " }
+    Write-Debug "Index de $ParameterSetName : $Index"
+    if ($List)
+    { $Command.ParameterSets[$Index].Parameters|Select -expand Name}
+    else 
+    { ,$Command.ParameterSets[$Index].Parameters }
+  }#process
+} # Get-ParameterSet
+
+function ConvertTo-VariableDeclaration {
+#Associé à la fonction New-TestSetParameters
+#Génére le code de déclaration des paramètres d'un jeux de paramètre d'une commande
+ [CmdletBinding()]
+ [OutputType([System.String])]
+ param (
+    [ValidateNotNullOrEmpty()]
+    [Parameter(Position=0,Mandatory=$true,ValueFromPipeline=$True)]
+   $ParameterSet,
+   
+    [ValidateNotNullOrEmpty()]
+    [Parameter(Position=1,Mandatory=$false)]
+  [scriptblock] $Code
+)       
+  
+  begin {  
+    [string[]]$CommonParameters=Get-CommonParameters
+  }
+  process {
+    $List=New-object System.Collections.Arraylist
+    Write-debug "Call ConvertTo-VariableDeclaration"
+    $ParameterSet|
+      Where { $CommonParameters -notContains $_.Name} |
+      foreach { 
+        $TypeName=$_.Parametertype
+        $RuntimeType=$TypeName -as [type]
+        write-debug $RuntimeType.fullname
+        try{ 
+         $Type=[Activator]::CreateInstance($RuntimeType) 
+        }catch {
+         $Type=$null
+         Write-Debug "Impossible de construire le type $($RuntimeType.fullname)"            
+        }
+    
+        write-debug ($Type -is [boolean])
+        write-debug ($Type -is [switch])
+        if ($Type -is [enum] )
+        { 
+           $ofs="','"
+           $List.Add( ('[string[]] ${0}=(''{1}'')' -f $_.Name,"$([enum]::GetValues($Type.GetType()))") ) > $null
+        }
+        elseif ( ($Type -is [boolean]) -or ($Type -is [switch]))
+        { 
+           $List.Add( ('${0}=@($true,$false)'-f $_.Name) ) > $null 
+        }
+        else
+        { 
+           $List.Add( ('[{0}] ${1}={2}' -f $TypeName,$_.Name,'''""''') ) > $null
+        }
+        if ($PSBoundParameters.ContainsKey('Code')) 
+        {  $List.Add("$Code") > $null }
+      }
+      ,$List
+   }#process
+}#ConvertTo-VariableDeclaration
+
+<#
+$Cmd=Get-Command Test-Path
+ #insére l'appel de la génération du cas de test
+ #pour les variables déclarées au moment de l'appel
+$BuildCall= {  $Result +=(New-TestSetParameters -command $Cmd -ParameterSetNames 'Path' -Exclude 'Credential','UseTransaction').Lines
+
+}
+ #Liste des paramètres communs
+[string[]]$Commons=Get-CommonParameters
+
+ #Récupére la liste des paramètres du cmdlet ou de la fonction
+ #Excepté les paramètres communs
+[string[]] $Parameters=$Cmd|Get-ParameterSet 'Path' -List|
+  Where { $Commons -notContains $_.Name}
+ 
+ #Supprime les variables de tests
+Remove-Variable $Parameters -ea SilentlyContinue 
+
+ #construit les lignes de déclaration des variables de test
+ #référencées par la fonction New-TestSetParameters
+$CodeLines=$Cmd|
+            Get-ParameterSet 'Path'|
+            ConvertTo-VariableDeclaration -code $BuildCall
+ 
+ #Ajoute l'initialisation de la collection            
+$CodeLines.Insert(0,'$Result=@()') > $null
+$CodeLines.Insert(0,'$Cmd=Get-Command Test-Path') > $null
+
+#Crée le fichier de génération du jeux de test
+$CodeLines > C:\Temp\InitVarTest.ps1
+
+ #Reste à éditer ce fichier afin de renseigner le contenu des variables 
+ # on peut également  réordonner l'ordre de déclaration des variables
+ #notamment placer les paramètre de type switch et de type énumération
+ #au début des déclarations, les combinaisons seront améliorées  
+ii C:\Temp\InitVarTest.ps1
+ 
+ #Puis on éxécute la génération du jeux de test 
+. "C:\Temp\InitVarTest.ps1"
+
+ #Le résultat
+$Result
+
+#Enfin l'objectif : insérez le code généré dans un script de test
 #>
