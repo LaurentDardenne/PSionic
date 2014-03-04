538e5d2f0ae8dbdadf46488a1e059711d85fd3c6
LaurentDardenne_cp <SND\LaurentDardenne_cp>
2014-03-04 18:46:21 +0000

    Ajout des fichiers de command Fr. Le paramètre -Online de Get-Help pointe sur l'aide HTML du site Codeplex

---
+++ trunk/Documentation/Helps/Add-ZipEntry/fr-FR.PsIonic.Add-ZipEntry.Cmds.ps1
@@ -0,0 +1,96 @@
+﻿# Add-ZipEntry command help
+@{
+	command = 'Add-ZipEntry'
+	synopsis = $Datas.AddZipEntrySynopsis
+	description = $Datas.AddZipEntryDescription
+	parameters = @{
+		Comment= $Datas.AddZipEntryParametersComment
+        EntryPathRoot = $Datas.AddZipEntryParametersEntryPathRoot
+		Name = $Datas.AddZipEntryParametersName
+		InputObject = $Datas.AddZipEntryParametersInputObject
+		Passthru = $Datas.AddZipEntryParametersPassthru
+		ZipFile = $Datas.AddZipEntryParametersZipFile
+	}
+	inputs = @(
+		@{
+			type = ''
+			description = $Datas.AddZipEntryInputsDescription1
+		}
+	)
+	outputs = @(
+		@{
+			type = 'Ionic.Zip.ZipEntry'
+			description = $Datas.AddZipEntryOutputsDescriptionIonicZipZipEntry
+		}
+	)
+	notes = $Datas.AddZipEntryNotes
+	examples = @(
+		@{
+			code = {
+try {         
+  $ZipFile=Get-Zipfile -Path C:\Temp\Test.zip
+  $File=Get-Item C:\Temp\Test.ps1 
+  Add-ZipEntry -InputObject $File -ZipFile $ZipFile
+} finally {
+  $ZipFile.Close()
+}
+			}
+			remarks = $Datas.AddZipEntryExamplesRemarks1
+			test = { . $args[0] }
+		}
+		@{
+			code = {
+try {         
+  $ZipFile=Get-Zipfile -Path C:\Temp\Test.zip
+  Get-ChildItem *.txt|Add-ZipEntry -ZipFile $ZipFile
+} finally {
+  $ZipFile.Close()
+}
+			}
+			remarks = $Datas.AddZipEntryExamplesRemarks2
+			test = { . $args[0] }
+		}
+		@{
+			code = {
+try {         
+  $ZipFile=Get-Zipfile -Path C:\Temp\Test.zip
+  [string]$Text=Get-Content C:\Temp\Test.ps1|Out-String
+  Add-ZipEntry -InputObject $Text -Name MyText -ZipFile $ZipFile
+} finally {
+  $ZipFile.Close()
+}
+			}
+			remarks = $Datas.AddZipEntryExamplesRemarks3
+			test = { . $args[0] }
+		}
+		@{
+			code = {
+try {
+  $ZipFile=Get-Zipfile -Path C:\Temp\Test.zip         
+  ConvertTo-CliXml $PSVersionTable | Add-ZipEntry -Name 'PSVersiontable.climxl' -ZipFile $ZipFile
+} finally {
+  $ZipFile.Close()
+}
+			}
+			remarks = $Datas.AddZipEntryExamplesRemarks4
+			test = { . $args[0] }
+		}
+		@{
+			code = {
+try {         
+  $ZipFile=Get-Zipfile -Path C:\Temp\Test.zip 
+  [byte[]] $Array=@(1..20)
+  Add-ZipEntry -InputObject $Array -Name MyArray -ZipFile $ZipFile
+} finally {
+  $ZipFile.Close()
+}
+			}
+			remarks = $Datas.AddZipEntryExamplesRemarks5
+			test = { . $args[0] }
+		}
+	)
+	links = @(
+ @{ text = ''; URI = 'https://psionic.codeplex.com/wikipage?title=Add-ZipEntry-FR'}
+	)
+}
+
---
+++ trunk/Documentation/Helps/ConvertFrom-CliXml/fr-FR.PsIonic.ConvertFrom-CliXml.Cmds.ps1
@@ -0,0 +1,36 @@
+﻿# ConvertFrom-CliXml command help
+@{
+	command = 'ConvertFrom-CliXml'
+	synopsis = $Datas.ConvertFromCliXmlSynopsis
+	description = $Datas.ConvertFromCliXmlDescription
+	parameters = @{
+		InputObject = $Datas.ConvertFromCliXmlParametersInputObject
+	}
+	inputs = @(
+		@{
+			type = ''
+			description = $Datas.ConvertFromCliXmlInputsDescription1
+		}
+	)
+	outputs = @(
+		@{
+			type = ''
+			description = $Datas.ConvertFromCliXmlOutputsDescription1
+		}
+	)
+	notes = $Datas.ConvertFromCliXmlNotes
+	examples = @(
+		@{
+			#title = ''
+			#introduction = ''
+			code = {
+			}
+			remarks = $Datas.ConvertFromCliXmlExamplesRemarks1
+			test = { . $args[0] }
+		}
+	)
+	links = @(
+ @{ text = ''; URI = 'https://psionic.codeplex.com/wikipage?title=ConvertFrom-CliXml-FR'}
+	)
+}
+
