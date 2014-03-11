6ebf7ec780be1e83b70b2e6c3f171380bd0d8d2f
LaurentDardenne_cp <SND\LaurentDardenne_cp>
2014-03-11 18:14:35 +0000

    Ajout de la méthode synthétique 'PSDebug' sur le type Le type log4net.Core.LogImpl. Modification : les propriéties contextuelle globalees 'Owner', 'RunspaceId', 'JobName' sont déplacées dans le module Log4Posh.

--- trunk/Modules/Log4Posh/Log4Posh.psm1
+++ trunk/Modules/Log4Posh/Log4Posh.psm1
@@ -7,6 +7,50 @@ Import-LocalizedData -BindingVariable Log4PoshMsgs -Filename Log4PoshLocalizedDa
  #On permet un chargement pour la V3 ( fx 4.0)
 Add-Type -Path "$psScriptRoot\$($PSVersionTable.PSVersion)\log4net.dll" 
 
+Function Get-ParentProcess {
+#Permet de retrouver le process parent ayant exécuté 
+#la session Powershell exécutant ce script/module
+ param( $ID )
+ $parentID=$ID         
+ $Result=@(
+   Do {
+     $Process=Get-WmiObject Win32_Process -Filter "ProcessID='$parentID'" -property Name,CommandLine,ParentProcessID
+      #Permet de retrouver la tâche appelante
+      #On peut inclure le nom de la tâche dans la ligne d'appel :
+      # ... -Command "$TaskName='TaskCeraKioskMonitorOFF_V1';..."
+     #$Logger.DebugFormat("Name :{0}`t cmdLine={1}",@($Process.name,$Process.CommandLine))
+     $parentID=$Process.ParentProcessID
+     try {
+      write-warning $parentID
+      get-process -ID $parentID
+      $exit=$true
+      }
+     catch [Microsoft.PowerShell.Commands.ProcessCommandException] {
+      $exit=$false       
+     }
+   } until ($Exit)
+ )
+   
+ $ofs='.'
+ [Array]::Reverse($Result)
+ ,$Result
+} #Get-ParentProcess
+
+ $Hostname=$ExecutionContext.host.Name
+ if ($Hostname -eq 'ServerRemoteHost')
+ {$_pid= (Get-ParentProcess  $PID)[0].Id}
+ else
+ {$_pid= $pid}
+ #Propriété statique, indique le process PowerShell courant
+ #Dans un job local ce n'est pas le process courant, mais le parent 
+[log4net.GlobalContext]::Properties.Item("Owner")=$_pid
+[log4net.GlobalContext]::Properties.Item("RunspaceId")=$ExecutionContext.host.Runspace.InstanceId
+
+ #Propriété dynamique, Log4net appel la méhtode ToString de l'objet référencé.
+$Script:JobName= new-object System.Management.Automation.PSObject -Property @{Value=$ExecutionContext.host.Name}
+$Script:JobName|Add-Member -Force -MemberType ScriptMethod ToString { $this.Value.toString() }
+[log4net.GlobalContext]::Properties["JobName"]=$Script:JobName
+
 $LogShortCut=@{
   LogManager = [log4net.LogManager];
   LogBasicCnfg = [log4net.Config.BasicConfigurator];
@@ -110,10 +154,10 @@ Function Start-ConsoleAppender {
  )
 
  # ------------- Type Accelerators -----------------------------------------------------------------
-function Get-Log4NetShorcuts{
+function Get-Log4NetShorcuts {
   #Affiche les raccourcis dédiés à Log4net
  $AcceleratorsType::Get.GetEnumerator()|Where {$_.Value.FullName -match "^log4net\.(.*)"}
-}
+}#Get-Log4NetShorcuts
  
 $AcceleratorsType = [PSObject].Assembly.GetType("System.Management.Automation.TypeAccelerators")   
  # Ajoute les raccourcis de type    
@@ -134,7 +178,7 @@ $AcceleratorsType = [PSObject].Assembly.GetType("System.Management.Automation.Ty
 
 #------------------------ Code -------------------------------------------
 
-function Set-Log4NetBasicConfigurator{
+function Set-Log4NetBasicConfigurator {
   Param (   
     [Parameter(Position=0,
                Mandatory=$True,
@@ -156,17 +200,16 @@ function Set-Log4NetBasicConfigurator{
       { [log4net.Config.BasicConfigurator]::Configure($Appender) }
 	  $Appender
   }
-}
-
+}#Set-Log4NetBasicConfigurator
 
 # -------------  Logger ----------------------------------------------------------------------------
-function Get-Log4NetLoggers{
+function Get-Log4NetLoggers {
   #Renvoi tous les loggers du repository par défaut
   #The root logger is not included in the returned array. 
  [log4net.LogManager]::GetCurrentLoggers()
-}
+}#Get-Log4NetLoggers
 
-function Get-Log4NetLogger{
+function Get-Log4NetLogger {
   Param (   
     [Parameter(Position=0,
                  Mandatory=$True,
@@ -179,9 +222,9 @@ function Get-Log4NetLogger{
  
   process
   {  [log4net.LogManager]::GetLogger($Name)  }
-}
+} #Get-Log4NetLogger
 
-function Get-Log4NetLoggerRepository{
+function Get-Log4NetLoggerRepository {
   Param (   
     [Parameter(Position=0,
                  Mandatory=$True,
@@ -197,7 +240,7 @@ function Get-Log4NetLoggerRepository{
  process {
    [log4net.LogManager]::GetLogger($RepositoryName,$Name)
  }
-}
+} #Get-Log4NetLoggerRepository
 
 function Get-Log4NetRootLogger {
   Param (   
@@ -216,9 +259,9 @@ function Get-Log4NetRootLogger {
  else 
   {$Repository=$Logger.Repository }
  $Repository.Root
-}
+} #Get-Log4NetRootLogger
 
-Function Set-LogFileName {
+Function Set-Log4NetFileName {
 #Change le nom de fichier d'un FileAppender 
  param (
      [ValidateNotNullOrEmpty()]
@@ -242,7 +285,7 @@ Function Set-LogFileName {
  }
 else 
  { Write-Error "L'appender de nom $AppenderName n'existe pas."}  
-}
+}#Set-Log4NetFileName
 
 # ----------- Suppression des objets du Wrapper -------------------------------------------------------------------------
 function OnRemoveLog4Posh {
@@ -274,7 +317,9 @@ $F=@(
   'Get-Log4NetLoggers',
   'Get-Log4NetLogger',
   'Get-Log4NetLoggerRepository',
-  'Get-Log4NetRootLogger'
+  'Get-Log4NetRootLogger',
+  'Set-Log4NetFileName',
+  'Get-ParentProcess'
 )
 
 #<DEFINE %DEBUG%>
@@ -283,5 +328,4 @@ Set-Alias -name dbglvl         -value Set-Log4NETDebugLevel
 Set-Alias -name sca            -value Stop-ConsoleAppender
 #<UNDEF %DEBUG%> 
 
-Export-ModuleMember -Variable LogDefaultColors  -Alias * -Function $F
-
+Export-ModuleMember -Variable LogDefaultColors,JobName  -Alias * -Function $F
