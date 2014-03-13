a9c33166ad65b6857b833411354895f87277bb8b
MatthewBETTON_cp <SND\MatthewBETTON_cp>
2014-03-13 08:50:15 +0000

    MAJ aide en-US

---
+++ trunk/Documentation/Helps/ConvertTo-CliXml/en-US.PsIonic.ConvertTo-CliXml.Cmds.ps1
@@ -0,0 +1,37 @@
+﻿
+# ConvertTo-CliXml command help
+@{
+	command = 'ConvertTo-CliXml'
+	synopsis = $Datas.ConvertToCliXmlSynopsis
+	description = $Datas.ConvertToCliXmlDescription
+	parameters = @{
+		InputObject = $Datas.ConvertToCliXmlParametersInputObject
+	}
+	inputs = @(
+		@{
+			type = ''
+			description = $Datas.ConvertToCliXmlInputsDescription1
+		}
+	)
+	outputs = @(
+		@{
+			type = ''
+			description = $Datas.ConvertToCliXmlOutputsDescription1
+		}
+	)
+	notes = $Datas.ConvertToCliXmlNotes
+	examples = @(
+		@{
+			#title = ''
+			#introduction = ''
+			code = {
+			}
+			remarks = $Datas.ConvertToCliXmlExamplesRemarks1
+			test = { . $args[0] }
+		}
+	)
+	links = @(
+ @{ text = ''; URI = 'https://psionic.codeplex.com/wikipage?title=ConvertTo-CliXml-EN'}
+	)
+}
+
