145bedcff73538f4b6bf3d63044defafcb5276fd
MatthewBETTON_cp <SND\MatthewBETTON_cp>
2014-03-26 20:24:42 +0000

    MAJ aide en-US

--- trunk/Documentation/Helps/Get-ZipFile/en-US.PsIonic.Get-ZipFile.Datas.ps1
+++ trunk/Documentation/Helps/Get-ZipFile/en-US.PsIonic.Get-ZipFile.Datas.ps1
@@ -1,7 +1,11 @@
 ﻿# Get-ZipFile command data
 $Datas = @{
-	GetZipFileSynopsis = 'todo'
-	GetZipFileDescription = ''
+	GetZipFileSynopsis = 'Gets and Sets an archive object from a Zip file.'
+	GetZipFileDescription = @"
+Gets an archive object from a Zip file. The archive object contains only the catalog, an objects list of type ZipEntry, in order to get the contents of a catalog entry. You need to expand it on a filesystem drive.
+Also, this function sets the most common properties of the object archive.
+The returned object is locked until you don't call the Close() method. On the other side, the use of the -List parameter does not lock the archive.
+"@
 	GetZipFileSetsManualOption = ''
 	GetZipFileSetsReadOption = ''
 	GetZipFileParametersEncoding =@"
@@ -19,37 +23,51 @@ For better portability, the default value ('DefaultEncoding') is recommended.
 	GetZipFileParametersEncryption = @"
 Encryption algorythm used for compression. Need to specify a password with the '-Password' parameter.
 "@
-	GetZipFileParametersList = ''
-	GetZipFileParametersProgressID = ''
+	GetZipFileParametersList = @"
+Gets the contained entries from the archive catalog. The returned objects are custom objects with the PSZipEntry type name.
+Aside from its property 'Info', all are read-only.
+The archive is not locked.
+"@
+	GetZipFileParametersProgressID = @"
+This parameter makes an events manager for read operations.
+When opening a zip archive with a big size, you can choose to display a default progress bar.
+This ID allows to distinguish the internal progress bar from the others. Use this parameter when you create more than one progress bar.
+"@
 	GetZipFileParametersPath = @"
-File name to retrieve. Can be a file object or a string. In this last case, generic characters can be used (* , ? , [A-D] ou [1CZ]).
+File name(s) to read. Can be a file names with generic characters (* , ? , [A-D] ou [1CZ]).
 "@
-	GetZipFileParametersOptions = ''
-	GetZipFileParametersPassword = @"
-Password used for encryption. Encryption method needs to be specified with the '-Encryption' parameter.
-"@
+	GetZipFileParametersOptions = 'Options used during the creation of the self-extracting archive (See New-ZipSfxOptions for more details).'
+	GetZipFileParametersPassword = 'Password used to read the Zip archive.'
 	GetZipFileParametersReadOptions = 'Options applied when reading archive (.zip). Created options from the New-ReadOptions function.'
-	GetZipFileParametersSortEntries = @"
-Entries are sorted before saving them. This can slow down the compression process, according to the number of files to compress.
-"@ 
+	GetZipFileParametersSortEntries = 'Entries are sorted.'  
 	GetZipFileParametersUnixTimeFormat = 'Unix time format for date file format will be used.'
 	GetZipFileParametersWindowsTimeFormat = 'Windows time format for date file format will be used.'
-	GetZipFileParametersZipErrorAction = @"
-Specify the error mode handling.
-.
-'Skip' value displays the error message and continue executing.
-.
-'InvokeErrorEvent' value can be only used when saving archive. Errors triggered while building catalog won't call error manager linked with this value.
-In this last case, a simple error will still be generated.
-.
-'Throw' value displays the error message and stops executing.
-.
-Avoid the use of the 'Retry' value because it could indefinitely try to solve a persistent error.
-"@
-	GetZipFileInputsDescription1 = ''
+	GetZipFileParametersZipErrorAction = 'Specify the error mode handling.'
+	GetZipFileInputsDescription1 = 'String'
 	GetZipFileOutputsDescriptionIonicZipZipFile = 'Ionic.Zip.ZipFile'
-	GetZipFileNotes = ''
-	GetZipFileExamplesRemarks1 = ''
+	GetZipFileNotes = @"
+This function returns only one Zip archive object from his fullname. Most of the parameters are used to configure the archive properties if you want to modify it.
+For example, the 'Encryption' parameter does not affect reading the Zip. Only the 'Password' parameter is needed, because this is entries that are encrypted, and not the object of type [Zipfile].
+So a Zip archive can theoretically contains several entries with differents compression mode for each of them.
+Pay attention that for each returned object you must call the Close() method in order to properly release resources archive.
+"@
+	GetZipFileExamplesRemarks1 = 'This example reads a Zip file, display its contents from a file and then releases resources archive.'
+	GetZipFileExamplesRemarks2 = 'This example reads a Zip file, adds an entry from a file to it, saves it and then releases resources archive.'
+	GetZipFileExamplesRemarks3 = 'This example reads a Zip file, adds an entry from a string, saves it and then releases resources archive.'
+	GetZipFileExamplesRemarks4 = @"
+This example reads a Zip file, adds .txt files fro ma folder to it, saves it and then releases resources archive.
+The Close() method internally calls the method Save() then PSDispose().
+"@
+	GetZipFileExamplesRemarks5 = @"
+The first instruction returns the catalog entries as objects of type PSZipEntry.
+The following search in the original catalog for an entry named 'Test.ps1' then decompresses the C:\Temp Folder.
+
+The difference between the two approaches is that objects of type PSZipEntry lacks methods specific to objects of type ZipEntry.
+"@
+	GetZipFileExamplesRemarks6 = @"
+This example opens an archive, delete all the .TXT files and adds all .LOG files from C:\Temp folder.
+Note that each change is recorded in the catalog of the archive, but only the call to Close () method will record the changes in the archive.
+"@
 }
 
 
