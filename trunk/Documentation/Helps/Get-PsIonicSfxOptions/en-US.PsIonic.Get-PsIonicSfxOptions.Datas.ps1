81ed2b68c9da43147fc87e1f1571111032c2f2a5
MatthewBETTON_cp <SND\MatthewBETTON_cp>
2014-03-26 17:49:01 +0000

    MAJ aide en-US

--- trunk/Documentation/Helps/Get-PsIonicSfxOptions/en-US.PsIonic.Get-PsIonicSfxOptions.Datas.ps1
+++ trunk/Documentation/Helps/Get-PsIonicSfxOptions/en-US.PsIonic.Get-PsIonicSfxOptions.Datas.ps1
@@ -1,12 +1,12 @@
 ﻿# Get-PsIonicSfxOptions command data
 $Datas = @{
-	GetPsIonicSfxOptionsSynopsis = 'todo'
-	GetPsIonicSfxOptionsDescription = ''
+	GetPsIonicSfxOptionsSynopsis = 'Returns a default configuration copy used when building a self-extracting archive.'
+	GetPsIonicSfxOptionsDescription = 'Returns a default configuration copy used when building a self-extracting archive. This default options set is used internally from differents public functions of Psionic module.'
 	GetPsIonicSfxOptionsSets__AllParameterSets = ''
-	GetPsIonicSfxOptionsInputsDescription1 = ''
-	GetPsIonicSfxOptionsOutputsDescription1 = ''
+	GetPsIonicSfxOptionsInputsDescription1 = 'No input'
+	GetPsIonicSfxOptionsOutputsDescription1 = 'Ionic.Zip.SelfExtractorSaveOptions'
 	GetPsIonicSfxOptionsNotes = ''
-	GetPsIonicSfxOptionsExamplesRemarks1 = ''
+	GetPsIonicSfxOptionsExamplesRemarks1 = 'This instruction returns the default configuration currently set.'
 }
 
 
