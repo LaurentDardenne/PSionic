77994ef25324570e2e6f697c66bb1b7bbdf9b2c9
MatthewBETTON_cp <SND\MatthewBETTON_cp>
2014-03-26 17:43:40 +0000

    MAJ aide en-US

--- trunk/Documentation/Helps/Format-ZipFile/en-US.PsIonic.Format-ZipFile.Datas.ps1
+++ trunk/Documentation/Helps/Format-ZipFile/en-US.PsIonic.Format-ZipFile.Datas.ps1
@@ -1,14 +1,51 @@
 ﻿# Format-ZipFile command data
 $Datas = @{
-	FormatZipFileSynopsis = 'todo'
-	FormatZipFileDescription = ''
+	FormatZipFileSynopsis = 'Formats contained properties of an archive object.'
+	FormatZipFileDescription = 'Formats contained properties of a Zip archive. Read Notes section for details about default displayed properties.'
 	FormatZipFileSets__AllParameterSets = ''
-	FormatZipFileParametersProperties = ''
-	FormatZipFileParametersZip = ''
-	FormatZipFileInputsDescription1 = ''
-	FormatZipFileOutputsDescription1 = ''
-	FormatZipFileNotes = ''
-	FormatZipFileExamplesRemarks1 = ''
+	FormatZipFileParametersProperties = 'Array of property names to display when internal calling to Format-List.'
+	FormatZipFileParametersZip = 'Archive object to format. You need to force the inclusion of the object by prefixing the variable with a comma.'
+	FormatZipFileInputsDescription1 = 'Ionic.Zip.ZipFile'
+	FormatZipFileOutputsDescription1 = 'Microsoft.PowerShell.Commands.Internal.Format'
+	FormatZipFileNotes = @"
+List of default properties to display :
+  CaseSensitiveRetrieval
+  UseUnicodeAsNecessary
+  UseZip64WhenSaving
+  RequiresZip64
+  OutputUsedZip64
+  InputUsesZip64
+  ProvisionalAlternateEncoding
+  AlternateEncoding
+  AlternateEncodingUsage
+  StatusMessageTextWriter
+  TempFileFolder
+  Password
+  ExtractExistingFile
+  ZipErrorAction
+  Encryption
+  SetCompression
+  MaxOutputSegmentSize
+  NumberOfSegmentsForMostRecentSave
+  ParallelDeflateThreshold
+  ParallelDeflateMaxBufferPairs
+  Count
+  FullScan
+  SortEntriesBeforeSaving
+  AddDirectoryWillTraverseReparsePoints
+  BufferSize
+  CodecBufferSize
+  FlattenFoldersOnExtract
+  Strategy
+  Name
+  CompressionLevel
+  CompressionMethod
+  Comment
+  EmitTimesInWindowsFormatWhenSaving
+.
+The presence of this function is due to a bug in the type extension system and formatting that can not manage a class with an indexer.
+"@
+	FormatZipFileExamplesRemarks1 = 'This example gets an archive object from a file name, then formats his contents before freeing system resources.'
 }
 
 
