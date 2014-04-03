6e09c55d8caea9100605f3a7f05b3b91ea96522d
LaurentDardenne_cp <SND\LaurentDardenne_cp>
2014-04-03 16:14:20 +0000

    Suppression du GUIDAttribute

--- trunk/Bin/AssemblyInfo.cs
+++ trunk/Bin/AssemblyInfo.cs
@@ -19,9 +19,6 @@ using System.Runtime.InteropServices;
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("64aa618b-09ff-41b3-81b4-07b5964a3039")]
-
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
