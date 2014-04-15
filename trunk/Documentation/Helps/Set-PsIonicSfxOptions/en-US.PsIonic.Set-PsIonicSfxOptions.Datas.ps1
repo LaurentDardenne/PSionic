1097541df7d56bf2c13c38cbbc22581c40af7ae0
MatthewBETTON_cp <SND\MatthewBETTON_cp>
2014-04-15 07:50:40 +0000

    MAJ aide en-US

--- trunk/Documentation/Helps/Set-PsIonicSfxOptions/en-US.PsIonic.Set-PsIonicSfxOptions.Datas.ps1
+++ trunk/Documentation/Helps/Set-PsIonicSfxOptions/en-US.PsIonic.Set-PsIonicSfxOptions.Datas.ps1
@@ -1,13 +1,13 @@
 ﻿# Set-PsIonicSfxOptions command data
 $Datas = @{
-	SetPsIonicSfxOptionsSynopsis = 'todo'
-	SetPsIonicSfxOptionsDescription = ''
+	SetPsIonicSfxOptionsSynopsis = 'Assigns new values ​​to the default configuration.'
+	SetPsIonicSfxOptionsDescription = 'Assigns new values ​​to the default configuration by copying the values ​​of the configuration shown in the default configuration.'
 	SetPsIonicSfxOptionsSets__AllParameterSets = ''
-	SetPsIonicSfxOptionsParametersOptions = ''
-	SetPsIonicSfxOptionsInputsDescription1 = ''
-	SetPsIonicSfxOptionsOutputsDescription1 = ''
+	SetPsIonicSfxOptionsParametersOptions = 'Contains the new values ​​to be assigned to the default configuration.'
+	SetPsIonicSfxOptionsInputsDescription1 = 'Ionic.Zip.SelfExtractorSaveOptions'
+	SetPsIonicSfxOptionsOutputsDescription1 = 'No.'
 	SetPsIonicSfxOptionsNotes = ''
-	SetPsIonicSfxOptionsExamplesRemarks1 = ''
+	SetPsIonicSfxOptionsExamplesRemarks1 = 'Those instructions create a SFX configuration with default values except for the Copyright field. Then it assigns new values ​​to the default configuration.'
 }
 
 
