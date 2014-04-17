9937927b0147eeedaf5637570933ed8f2b96adae
MatthewBETTON_cp <SND\MatthewBETTON_cp>
2014-04-17 07:31:22 +0000

    MAJ aide en-US

--- trunk/Documentation/Helps/ConvertFrom-CliXml/en-US.PsIonic.ConvertFrom-CliXml.Datas.ps1
+++ trunk/Documentation/Helps/ConvertFrom-CliXml/en-US.PsIonic.ConvertFrom-CliXml.Datas.ps1
@@ -1,13 +1,17 @@
 ﻿# ConvertFrom-CliXml command data
 $Datas = @{
-	ConvertFromCliXmlSynopsis = 'todo'
-	ConvertFromCliXmlDescription = ''
+	ConvertFromCliXmlSynopsis = 'Creates a Powershell object from an XML representation and issues it in the pipeline.'
+	ConvertFromCliXmlDescription = @"
+You can receive an XML object from the 'Expand-ZipEntry' function.
+"@
 	ConvertFromCliXmlSets__AllParameterSets = ''
-	ConvertFromCliXmlParametersInputObject = ''
+	ConvertFromCliXmlParametersInputObject = 'Specifies XML representation of the object to be converted.'
 	ConvertFromCliXmlInputsDescription1 = ''
 	ConvertFromCliXmlOutputsDescription1 = ''
 	ConvertFromCliXmlNotes = ''
-	ConvertFromCliXmlExamplesRemarks1 = ''
+	ConvertFromCliXmlExamplesRemarks1 = @'
+At first time, those instructions save a serialized object into an entry named 'PSVersiontable_clixml', then in a second time link it and assign it to the $MyVersionArray variable.
+'@
 }
 
 
