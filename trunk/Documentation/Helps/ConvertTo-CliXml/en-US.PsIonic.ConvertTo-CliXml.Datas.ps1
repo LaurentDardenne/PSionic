835430c4a7c24cbd7d16879e7db54915e9578791
MatthewBETTON_cp <SND\MatthewBETTON_cp>
2014-04-17 07:39:45 +0000

    MAJ aide en-US

--- trunk/Documentation/Helps/ConvertTo-CliXml/en-US.PsIonic.ConvertTo-CliXml.Datas.ps1
+++ trunk/Documentation/Helps/ConvertTo-CliXml/en-US.PsIonic.ConvertTo-CliXml.Datas.ps1
@@ -1,13 +1,20 @@
 ﻿# ConvertTo-CliXml command data
 $Datas = @{
-	ConvertToCliXmlSynopsis = 'todo'
-	ConvertToCliXmlDescription = ''
+	ConvertToCliXmlSynopsis = 'Creates an XML representation of one or several objects and sends it to the pipeline.'
+	ConvertToCliXmlDescription = @"
+You can sends the result of this function to the 'Add-ZipEntry' and 'Update-ZipEntry' functions.
+"@
 	ConvertToCliXmlSets__AllParameterSets = ''
-	ConvertToCliXmlParametersInputObject = ''
+	ConvertToCliXmlParametersInputObject = 'Specifies the object to be converted.'
 	ConvertToCliXmlInputsDescription1 = ''
 	ConvertToCliXmlOutputsDescription1 = ''
-	ConvertToCliXmlNotes = ''
-	ConvertToCliXmlExamplesRemarks1 = ''
+	ConvertToCliXmlNotes = "The record of the XML representation is compatible with the 'Export-CliXML' Cmdlet."
+	ConvertToCliXmlExamplesRemarks1 = @'
+Those commands add a named entry to the 'C:\Temp\Test.zip' archive from  a serialized object:
+   - The first instruction creates an archive object from a file name,
+   - The second one serializes the object contained in the $PSVersionTable variable and adds it as an input named in the archive,
+   - The third saves the archive to the disk and frees system ressources.
+'@
 }
 
 
