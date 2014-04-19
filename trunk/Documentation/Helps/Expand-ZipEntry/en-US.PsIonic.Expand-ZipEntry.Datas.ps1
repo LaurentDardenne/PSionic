8f29e71b9fd76c9c338428515146dd180d12052c
MatthewBETTON_cp <SND\MatthewBETTON_cp>
2014-04-19 08:34:06 +0000

    MAJ aide en-US

--- trunk/Documentation/Helps/Expand-ZipEntry/en-US.PsIonic.Expand-ZipEntry.Datas.ps1
+++ trunk/Documentation/Helps/Expand-ZipEntry/en-US.PsIonic.Expand-ZipEntry.Datas.ps1
@@ -3,7 +3,7 @@ $Datas = @{
 	ExpandZipEntrySynopsis = 'Extracts entries from a compressed archive to Zip format.'
 	ExpandZipEntryDescription = @"
 Extracts entries from a compressed archive to a Zip format. Extracted data are either serialized data, either an array of bytes, or a string and it allows to build an object.
-To extract files from an archive use Expand-ZipFile Cmdlet.
+To extract files from an archive use 'Expand-ZipFile' Cmdlet.
 "@
 	ExpandZipEntrySetsByteArray = ''
 	ExpandZipEntrySetsString = ''
@@ -35,13 +35,13 @@ For better portability, the default value ('DefaultEncoding') is recommended.
 	ExpandZipEntryOutputsDescriptionSystemByte = ''
 	ExpandZipEntryNotes = 'This function is linked to the Add-ZipEntry function.'
 	ExpandZipEntryExamplesRemarks1 = @'
-At first time, those instructions save text to an entry named MyText, then links it and assigns it to the $MyVersionArray variable.
+At first time, those instructions save text to an entry named MyText, then links it and assigns it to the ''$MyVersionArray'' variable.
 '@
 	ExpandZipEntryExamplesRemarks2 =@'
-At first time, those instructions save serialized object to an entry named 'PSVersiontable_clixml', then links and assigns it to the $MyVersionArray variable. 
+At first time, those instructions save serialized object to an entry named ''PSVersiontable_clixml'', then links and assigns it to the $MyVersionArray variable. 
 '@
 	ExpandZipEntryExamplesRemarks3 =@'
-At first time, those instructions save an array of bytes to an entry named MyArray, then links it and assigns it to the $Array variable.
+At first time, those instructions save an array of bytes to an entry named ''MyArray'', then links it and assigns it to the ''$Array'' variable.
 '@
 }
 
