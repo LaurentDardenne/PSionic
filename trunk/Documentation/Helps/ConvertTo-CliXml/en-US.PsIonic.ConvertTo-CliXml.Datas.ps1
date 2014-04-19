01c68746d1a33f215200653b44602d3bdc513f29
MatthewBETTON_cp <SND\MatthewBETTON_cp>
2014-04-19 08:32:02 +0000

    MAJ aide en-US

--- trunk/Documentation/Helps/ConvertTo-CliXml/en-US.PsIonic.ConvertTo-CliXml.Datas.ps1
+++ trunk/Documentation/Helps/ConvertTo-CliXml/en-US.PsIonic.ConvertTo-CliXml.Datas.ps1
@@ -10,7 +10,7 @@ You can sends the result of this function to the 'Add-ZipEntry' and 'Update-ZipE
 	ConvertToCliXmlOutputsDescription1 = ''
 	ConvertToCliXmlNotes = "The record of the XML representation is compatible with the 'Export-CliXML' Cmdlet."
 	ConvertToCliXmlExamplesRemarks1 = @'
-Those commands add a named entry to the 'C:\Temp\Test.zip' archive from  a serialized object:
+Those commands add a named entry to the 'C:\Temp\Test.zip' archive from a serialized object:
    - The first instruction creates an archive object from a file name,
    - The second one serializes the object contained in the $PSVersionTable variable and adds it as an input named in the archive,
    - The third saves the archive to the disk and frees system ressources.
