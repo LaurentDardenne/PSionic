cc2d625da8a10f75de2b86de296a7b0a4774d426
MatthewBETTON_cp <SND\MatthewBETTON_cp>
2014-05-03 16:18:25 +0000

    MAJ aide en-US

--- trunk/Documentation/Helps/Reset-PsIonicSfxOptions/en-US.PsIonic.Reset-PsIonicSfxOptions.Datas.ps1
+++ trunk/Documentation/Helps/Reset-PsIonicSfxOptions/en-US.PsIonic.Reset-PsIonicSfxOptions.Datas.ps1
@@ -4,7 +4,7 @@ $Datas = @{
 	ResetPsIonicSfxOptionsDescription = 'Resets the default configuration used in the construction of a self-extracting archive. This default configuration is used internaly from public functions of the Psionic module, related to the management of self-extracting archive.'
 	ResetPsIonicSfxOptionsSets__AllParameterSets = ''
 	ResetPsIonicSfxOptionsInputsDescription1 = 'No value'
-	ResetPsIonicSfxOptionsOutputsDescription1 = 'No value. The default configuration now contains identical values to those of an object returned by the New-ZipSfxOptions function.'
+	ResetPsIonicSfxOptionsOutputsDescription1 = "No value. The default configuration now contains identical values to those of an object returned by the 'New-ZipSfxOptions' function."
 	ResetPsIonicSfxOptionsNotes = ''
 	ResetPsIonicSfxOptionsExamplesRemarks1 = 'This instruction resets the default configuration.'
 }
