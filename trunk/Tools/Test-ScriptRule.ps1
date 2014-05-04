b1bce557362a0e62e4ac6a1e69450b76cc5429a2
LaurentDardenne_cp <SND\LaurentDardenne_cp>
2014-05-04 17:03:01 +0000

    Ajout du contrôle de certaines régle de codage

---
+++ trunk/Tools/Test-ScriptRule.ps1
@@ -0,0 +1,38 @@
+﻿#todo
+# Add into build.ps1
+Function Test-ScriptRule{
+#Extract from Microsoft Script Analyzer, ISE Addon.
+ param(
+  [string] $FilePath,
+  [string[]] $Rules #nom -> dll/NS.Name : CheckInPolicy.IsAliasUsed
+ )
+
+   #Initialize or RAZ the Problem collection
+   #Create private AST from a script
+  $PSAnalyzer = new-object CheckInPolicy.PSAnalyzer($FilePath)
+   
+   #Ces classes renseignent les propriétés de $PSAnalyzer
+  new-object CheckInPolicy.IsAliasUsed >$null
+  new-object CheckInPolicy.CheckForEmptyCatchBlock >$null
+  new-object CheckInPolicy.PositionalArgumentsFound >$null
+  new-object CheckInPolicy.FunctionNameUseStandardVerbName >$null
+  new-object CheckInPolicy.InvokeExpressionFound >$null
+  
+  $pbCount=$PSAnalyzer.getProblemCount
+  if ($pbCount -gt 0)
+  {
+     for ($i = 0; $i -lt $pbCount; $i++)
+     {
+        $pSAnalyzer.GetProblem($i)
+     }
+  }
+}#Test-ScriptRule
+
+Add-Type -Path 'C:\Program Files (x86)\Microsoft Corporation\Microsoft Script Browser\CheckInPolicy.dll'
+
+$filePath="$PsIonicLivraison\PsIonic\PsIonic.psm1"
+[xml]$Datas=Gc "$PsIonicTools\ScriptAnalyzerRules.xml"
+$Rules=$Datas.rules.rule|select -ExpandProperty Name
+
+Test-ScriptRule -FilePath $FilePath -Rules $Rules|
+ Select-Object ID, Line, Name, Script,Statement 
