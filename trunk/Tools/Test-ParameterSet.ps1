153afb1e796481b603610a56957605515744b5c1
LaurentDardenne_cp <SND\LaurentDardenne_cp>
2014-08-07 17:29:28 +0000

    Adaptation pour Scriptcop

--- trunk/Tools/Test-ParameterSet.ps1
+++ trunk/Tools/Test-ParameterSet.ps1
@@ -8,14 +8,19 @@ Function Test-ParameterSet{
    Les noms de paramètres débutant par un chiffre invalideront le test.
 #>  
  param (
-   #Nom de la commande à tester
-  [parameter(Mandatory=$True,ValueFromPipeline=$True)]
-  [string]$Command
- ) 
+     #Nom de la commande à tester
+    [Parameter(ParameterSetName='TestCommandInfo',Mandatory=$true,ValueFromPipeline=$true)]
+    [Management.Automation.CommandInfo] $CommandInfo,
+     
+     #Si précisé, émet un PSobjet de type 'ParameterSetValidation, 
+     #sinon émet des erreurs si des données sont invalides.
+    [switch] $Passthru
+ )
+
 begin {
  [string[]] $CommonParameters=[System.Management.Automation.Internal.CommonParameters].GetProperties().Name
  
- function Test-Sequential{
+  Function Test-Sequential{
   #Prerequis: la collection doit être triée
   param([int[]]$List)
     $Count=$List.Count
@@ -25,7 +30,14 @@ begin {
        {return $false}
     }
     return $true
- }# Test-Sequential
+  }# Test-Sequential
+
+  Function FormatCommandInfoName {
+    if ($CommandInfo.ModuleName -eq [string]::Empty)
+    { "[{0}]" -F $CommandInfo.Name }
+    else 
+    { "[{0}\{1}]" -F $CommandInfo.ModuleName,$CommandInfo.Name }
+  }#FormatCommandInfoName
 
   Function New-ParameterSetValidation{
     param(
@@ -43,7 +55,7 @@ begin {
   
     [pscustomobject]@{
       PSTypeName='ParameterSetValidation';
-      #Nom du module (optionnel)
+       #Nom du module (optionnel)
       ModuleName=$ModuleName;
        #Nom de la fonction ou du cmdlet testé
       CommandName=$CommandName;
@@ -57,7 +69,7 @@ begin {
        #Détails des régles implémentées. 
       Report=$Report;
      }
-}# New-ParameterSetValidation
+ }# New-ParameterSetValidation
 
  Function New-ParameterSetReport{
   #Mémorise les informations.
@@ -65,7 +77,7 @@ begin {
     param(
        [Parameter(Mandatory=$True,position=0)]
        #Liste des paramètres du jeu courant
-      $Params,
+      $Parameters,
        [Parameter(Mandatory=$True,position=1)]
        #Liste des paramètres n'appartenant pas au jeu courant
       $Others,
@@ -81,7 +93,7 @@ begin {
       $isHasUniqueParameter,
        [Parameter(Mandatory=$True,position=5)]
         #les numéros de position ne doivent pas être dupliqués 
-      $isPositionContainsDuplicate,
+      $isPositionContainsDuplicated,
        [Parameter(Mandatory=$True,position=6)]
         #les numéros de position doivent se suivre séquentiellement
       $isPositionSequential,
@@ -94,62 +106,60 @@ begin {
        [Parameter(Mandatory=$True,position=9)]
        #Les noms de jeux de paramètre doivent être unique
        #PS autorise des noms de jeux sensible à la casse
-      $isParameterSetNameDuplicate
+       #bug: https://connect.microsoft.com/PowerShell/feedback/details/928085/parameterset-names-should-not-be-case-sensitive
+      $isParameterSetNameDuplicated
    )
   
     [pscustomobject]@{
       PSTypeName='ParameterSetReport';
-      Params=$Params;
+      Parameters=$Parameters;
       Others=$Others;
       Positions=$Positions;
       InvalidParameterName=$InvalidParameterName;
       isHasUniqueParameter=$isHasUniqueParameter;
-      isPositionContainsDuplicate=$isPositionContainsDuplicate;
+      isPositionContainsDuplicated=$isPositionContainsDuplicated;
       isPositionSequential=$isPositionSequential;
       isPositionValid=$isPositionValid;
       isContainsInvalidParameter=$isContainsInvalidParameter;
-      isParameterSetNameDuplicate=$isParameterSetNameDuplicate
+      isParameterSetNameDuplicated=$isParameterSetNameDuplicated
      }
   }# New-ParameterSetReport
 }#begin
 
 process {
-  $Cmd=Get-Command $Command
-  Write-Debug "Test $Command"
+  Write-Debug "Test $CommandInfo"
  
         #bug PS : https://connect.microsoft.com/PowerShell/feedback/details/653708/function-the-metadata-for-a-function-are-not-returned-when-a-parameter-has-an-unknow-data-type
   $oldEAP,$ErrorActionPreference=$ErrorActionPreference,'Stop'
-   $SetCount=$Cmd.get_ParameterSets().count
+   $SetCount=$CommandInfo.get_ParameterSets().count
   $ErrorActionPreference=$oldEAP
 
-  $_AllNames=@($Cmd.ParameterSets|
+  $_AllNames=@($CommandInfo.ParameterSets|
             Foreach {
               $PrmStName=$_.Name
               $P=$_.Parameters|Foreach {$_.Name}|Where  {$_ -notin $CommonParameters} 
               Write-Debug "Build $PrmStName $($P.Count)"
               if (($P.Count) -eq 0)
-              { Write-Warning "[$($Cmd.Name)]: the parameter set '$PrmStName' is empty." }
+              { Write-Warning "[$($CommandInfo.Name)]: the parameter set '$PrmStName' is empty." }
               $P
             })
 
-  $NamesOfParameterSet=$Cmd.ParameterSets.Name
-#   if ($_AllNames.Count -eq 0 ) 
-#   { return $Sets  }
+  $NamesOfParameterSet=$CommandInfo.ParameterSets.Name
    
    #Contient les noms des paramètres de tous les jeux
    #Les noms peuvent être dupliqués
   $AllNames=new-object System.Collections.ArrayList(,$_AllNames)
   $ParametersStartsWithNumber=new-object System.Collections.ArrayList
   
-  $Cmd.ParameterSets| 
+  $CommandInfo.ParameterSets| 
    foreach {
      $ParameterSetName=$_.Name 
      Write-Debug "Validate ParameterSet '$ParameterSetName'."
      
-     $isParameterSetNameDuplicate =@($NamesOfParameterSet -eq $ParameterSetName).Count -gt 1
+     $isParameterSetNameDuplicated =@($NamesOfParameterSet -eq $ParameterSetName).Count -gt 1
      
       #Contient tous les noms de paramètre du jeux courant
-     $Params=new-object System.Collections.ArrayList
+     $Parameters=new-object System.Collections.ArrayList
       #Contient les positions des paramètres du jeux courant
      $Positions=new-object System.Collections.ArrayList
      $Others=$AllNames.Clone()
@@ -159,7 +169,7 @@ process {
       Foreach {
         $ParameterName=$_.Name
         Write-debug "Add $ParameterName $($_.Position)"
-        $Params.Add($ParameterName) > $null
+        $Parameters.Add($ParameterName) > $null
         $Positions.Add($_.Position) > $null
          #Les constructions suivantes sont possibles, mais pas testées : ${der[nier}, ${-1}, etc.
         if ($ParameterName -match "^\d" )
@@ -171,7 +181,7 @@ process {
      
       #Supprime dans la collection globale
       #les noms de paramètres du jeux courant
-     $Params| 
+     $Parameters| 
       Foreach { 
         Write-Debug "Remove $_"
         $Others.Remove($_) 
@@ -181,10 +191,10 @@ process {
      $FilterPositions=$Positions|Where {$_ -ge 0}
       #Get-Unique attend une collection triée
      $SortedPositions=$FilterPositions|Sort-Object  
-     $isDuplicate= -not (@($SortedPositions|Get-Unique).Count -eq $FilterPositions.Count)
+     $isDuplicated= -not (@($SortedPositions|Get-Unique).Count -eq $FilterPositions.Count)
      $isSequential= Test-Sequential $SortedPositions
      
-     $isPositionValid=($isDuplicate -eq $False) -and ($isSequential -eq $true)
+     $isPositionValid=($isDuplicated -eq $False) -and ($isSequential -eq $true)
      
      $HasParameterUnique= &{
          if ($Others.Count -eq 0 ) 
@@ -192,7 +202,7 @@ process {
            Write-Debug "Only one parameter set."
            return $true
          }
-         foreach ($Current in $Params)
+         foreach ($Current in $Parameters)
          {
            if ($Current -notin $Others)
            { return $true}
@@ -202,23 +212,92 @@ process {
      
      $isContainsInvalidParameter=$ParametersStartsWithNumber.Count -gt 0
 
-     $isValid= $HasParameterUnique -and $isPositionValid -and -not $isContainsInvalidParameter -and -not $isParameterSetNameDuplicate
-     $Report=New-ParameterSetReport $Params `
+     $isValid= $HasParameterUnique -and $isPositionValid -and -not $isContainsInvalidParameter -and -not $isParameterSetNameDuplicated
+     $Report=New-ParameterSetReport $Parameters `
                                     $Others `
                                     $Positions `
                                     $ParametersStartsWithNumber `
                                     $HasParameterUnique `
-                                    $isDuplicate `
+                                    $isDuplicated `
                                     $isSequential `
                                     $isPositionValid `
                                     $isContainsInvalidParameter `
-                                    $isParameterSetNameDuplicate
+                                    $isParameterSetNameDuplicated
 
-     New-ParameterSetValidation  $Cmd.ModuleName `
-                                 $Cmd.Name `
-                                 $ParameterSetName `
-                                 $isValid `
-                                 $Report
+     $ObjectValidation=New-ParameterSetValidation  $CommandInfo.ModuleName `
+                                                   $CommandInfo.Name `
+                                                   $ParameterSetName `
+                                                   $isValid `
+                                                   $Report
+    Write-debug "$ObjectValidation=$ObjectValidation"
+    Write-debug "$ObjectValidation= $($ObjectValidation.Report)"
+    if ($Passthru) 
+    { $ObjectValidation }
+    elseif  ($ObjectValidation.isValid -eq $false)
+    {
+       #http://msdn.microsoft.com/en-us/library/dd878348.aspx
+      if ($Report.isHasUniqueParameter -eq $false)
+      { 
+        $ofs=','
+        $builder = new-Object System.Text.StringBuilder
+        $builder.AppendLine("$(FormatCommandInfoName) The parameter set '$ParameterSetName' dot not contains unique parameter.") > $null
+        $builder.AppendLine("A valid parameter set must have a single parameter does not belong to any other parameter set :") > $null
+        $builder.AppendLine(" Parameters = $($Report.Parameters)") > $null
+        $builder.AppendLine(" Others = $($Report.Others)") > $null
+        
+        $ErrorRecord = @{
+          Category="InvalidData"
+          ErrorId='TestParameterSet.NotContainsUniqueParameter' 
+          Message=$builder.ToString()
+          # On renseigne pas targetObject dans l'ErrorRecord, mais on insére le nom complet de la fonction 
+          # au début du message d'erreur.
+        }
+        Write-Error @ErrorRecord
+      } 
+      
+      if ($Report.isPositionContainsDuplicated)
+      { 
+        $ofs=','
+        $ErrorRecord = @{
+          Category="InvalidData"
+          ErrorId='TestParameterSet.PositionNumbersDuplicated'
+          Message="$(FormatCommandInfoName) The parameter set '$ParameterSetName'.`r`nThe position numbers should not be duplicated : $positions"
+        }
+        Write-Error @ErrorRecord
+      }
+      elseif ($Report.isPositionSequential -eq $false) 
+      { 
+        $ofs=','
+        $ErrorRecord = @{
+          Category="InvalidData"
+          ErrorId='TestParameterSet.PositionNumbersNotOrderedSequence'
+          Message="$(FormatCommandInfoName) The parameter set '$ParameterSetName'.`r`nThe position numbers must be an ordered sequence: $positions"
+        }
+        Write-Error @ErrorRecord
+      }
+      
+      if ($Report.isContainsInvalidParameter)
+      { 
+        $ofs="','"
+        $ErrorRecord = @{
+          Category="InvalidData"
+          ErrorId='TestParameterSet.ParameterNamesStartWithNumber'
+          Message="$(FormatCommandInfoName) The parameter set '$ParameterSetName' contains invalid paremeter names.`r`nThe parameter names should not start with a number : '$ParametersStartsWithNumber'"
+        }
+        Write-Error @ErrorRecord
+      } 
+      
+      if ($Report.isParameterSetNameDuplicated)
+      { 
+        $ofs="','"
+        $ErrorRecord = @{
+          Category="InvalidData"
+          ErrorId='TestParameterSet.ParameterSetNameDuplicated'
+          Message="$(FormatCommandInfoname) The parameter set '$ParameterSetName' should not be duplicated.`r`nThe parameterSet names are case sensitive : '$NamesOfParameterSet'"
+        }
+        Write-Error @ErrorRecord
+      }
+    }
    }#For ParameterSets
  }#process
 }#Test-ParameterSet
